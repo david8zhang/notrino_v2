60b561f964dbf7ec411fd4ec376e8085ef00f2b8
David Zhang <david8zhang@berkeley.edu>
2016-01-23 10:07:01 -0800

    added Questions routes

--- server/routes/routes.js
+++ server/routes/routes.js
@@ -2,6 +2,7 @@ var express = require('express');
 var router = express.Router();
 var userController = require('../controllers/users.js');
 var qpoolController = require('../controllers/question-pools.js');
+var questionController = require('../controllers/questions.js');
 
 //API Endpoints
 router.route('/users/register')
@@ -21,5 +22,14 @@ router.route('/questions/pools/create')
 
 router.route('/questions/pools/index')
 	.get(qpoolController.getQPool);
-	
+
+router.route('/questions/single/create')
+	.post(questionController.createQuestion);
+
+router.route('/questions/single/index')
+	.get(questionController.getQuestion);
+
+router.route('/questions/single/all')
+	.get(questionController.getQuestions);
+
 module.exports = router;
