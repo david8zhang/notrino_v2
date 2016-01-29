6935e94e882e855051eede1cbe6eb90748971cd9
David Zhang <david8zhang@berkeley.edu>
2016-01-28 22:24:25 -0800

    added some stuff

--- android-client/app/src/main/java/notrino/gcm_android/controllers/service/MyGcmListenerService.java
+++ android-client/app/src/main/java/notrino/gcm_android/controllers/service/MyGcmListenerService.java
@@ -1,9 +1,18 @@
 package notrino.gcm_android.controllers.service;
 
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
+import android.media.RingtoneManager;
+import android.net.Uri;
 import android.os.Bundle;
-import android.support.v7.app.NotificationCompat;
+import android.support.v4.app.NotificationCompat;
 
 import com.google.android.gms.gcm.GcmListenerService;
+
+import notrino.gcm_android.R;
+import notrino.gcm_android.views.activities.MainActivity;
 
 
 /**
@@ -15,11 +24,31 @@ public class MyGcmListenerService extends GcmListenerService {
 
     @Override
     public void onMessageReceived(String from, Bundle data) {
-        String message = data.getString("message");
-        sendNotification(message);
+        System.out.println("Message:" + data);
+        System.out.println("Key1:" + data.getString("key1"));
+        sendNotification(data.getString("key1"));
     }
 
     /** Show a toast, change this to a notification later. */
     private void sendNotification(String message) {
+
+        Intent intent = new Intent(this, MainActivity.class);
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+        PendingIntent pendingIntent = PendingIntent.getActivity(this, 0 /* Request code */, intent,
+                PendingIntent.FLAG_ONE_SHOT);
+
+        Uri defaultSoundUri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
+        NotificationCompat.Builder notificationBuilder = new NotificationCompat.Builder(this)
+                .setSmallIcon(R.drawable.cast_ic_notification_2)
+                .setContentTitle("GCM Message")
+                .setContentText(message)
+                .setAutoCancel(true)
+                .setSound(defaultSoundUri)
+                .setContentIntent(pendingIntent);
+
+        NotificationManager notificationManager =
+                (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+
+        notificationManager.notify(0 /* ID of notification */, notificationBuilder.build());
     }
 }
--- lambda/notrino_gcm/notrino_gcm.js
+++ lambda/notrino_gcm/notrino_gcm.js
@@ -6,8 +6,6 @@ var gcm = require('node-gcm');
 exports.handler = function(event, context) {
 	//Google cloud messaging
 	var API_KEY = "AIzaSyAd8KBTB4FZ-_18cCuf3q81Zsu6Yi_KElY";
-	var sendID = "578010136131"
-	var url = "https://gcm-http.googleapis.com/gcm/send";
 	var rest_url = "https://infinite-anchorage-62838.herokuapp.com/api/v1";
 	var srcBucket = event.Records[0].s3.bucket.name;
 
@@ -15,27 +13,17 @@ exports.handler = function(event, context) {
 	var srcKey = decodeURIComponent(event.Records[0].s3.object.key.replace(/\+/g, " "));
 	async.waterfall([
 		function upload(next) {
+			console.log("Key: " + srcKey);
 			var reg_token = "";
 			var client = new Client();
 			var args = {
 				parameters: {
 					user_id: srcKey 
 				}
-			};
-			client.get(rest_url + "/users/index", args, function(data, response) {
-				reg_token = data.Items[0].reg_token;
+			}
+			client.post(rest_url + "/gcm", function(data, response) {
+				console.log(data);
 			});
-			var reg_tokens = [reg_token];
-			var sender = new gcm.Sender(API_KEY);
-			sender.send(message, { registrationTokens: regTokens }, function(err, response) {
-				if(err) console.log(err);
-				else console.log(response);
-			});
-
-			sender.sendNoRetry(message, {topic: '/topic/global'}, function(err, response) {
-				if(err) console.log(err);
-				else console.log(response);
-			})
 
 		}
 	], function(err) {
