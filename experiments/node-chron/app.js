c3b679281d0c737f1cb2763c834a3cc3cbdea5c9
David Zhang <david8zhang@berkeley.edu>
2016-04-19 23:03:18 -0700

    created cron job scheduler node server

--- experiments/node-chron/app.js
+++ experiments/node-chron/app.js
@@ -5,12 +5,32 @@
 * @Author: David Zhang
 * @Description: A Cron-scheduler running on NodeJS Express Server
 **/
+var AWS = require('aws-sdk');
+var sha1 = require('sha-1');
+var docClient = new AWS.DynamoDB.DocumentClient({ region: 'us-west-1'});
 var express = require('express');
 var app = express();
 var cron = require('cron');
 var port = process.env.PORT || 8800;
-var scheduledTask = require('./controllers/scheduledController.js');
-var cronJob = cron.job("0 * * * * *", scheduledTask.task("new task!"));
+var cronJob = cron.job("* * * * * *", function() {
+    var params = {};
+    params.TableName = 'scheduled-task';
+    var task_id = sha1(Math.floor(Date.now()) / 1000).toString();
+    var task_name = "Hello world!";
+    var timestamp = Math.floor(Date.now() / 1000).toString();
+    params.Item = {
+        task_id: task_id,
+        task_name: task_name,
+        timestamp: timestamp
+    };
+    docClient.put(params, function(err, data) {
+        if(err) {
+            console.log(err);
+        } else {
+            console.log("Success!");
+        }
+    })
+});
 cronJob.start();
 
 app.listen(port);
